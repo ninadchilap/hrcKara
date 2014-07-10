6b81fc47260f65433046eb1070c548fafe433e4b
ninad chilap <ninadchilap@gmail.com>
2014-07-10 13:36:35 +0530

    new welcome screen

--- src/com/nino/hrckaraoke/Welcome.java
+++ src/com/nino/hrckaraoke/Welcome.java
@@ -1,25 +1,30 @@
 package com.nino.hrckaraoke;
 
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.widget.VideoView;
 
 public class Welcome extends Activity {
 	
-	VideoView hrc;
+	NewVideoView hrc;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcome);
 		
-		hrc=(VideoView)findViewById(R.id.videoView1);
-		String path = "android.resource://com.nino.hrckaraoke/raw/famous";
+		hrc=(NewVideoView)findViewById(R.id.videoView1);
+		String path = "android.resource://com.nino.hrckaraoke/raw/hrckara2";
 		hrc.setVideoURI(Uri.parse(path));		
 		hrc.start();
-		
+		hrc.setOnPreparedListener (new OnPreparedListener() {                    
+		    @Override
+		    public void onPrepared(MediaPlayer mp) {
+		        mp.setLooping(true);
+		    }
+		});
 	}
 
 	
