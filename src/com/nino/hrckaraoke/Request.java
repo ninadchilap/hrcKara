b457654c23f141c7f6ecc6659838b10c7feb5d17
Ninad Chilap <ninadchilap@gmail.com>
2014-07-13 23:05:36 +0530

    Changed tab button

--- src/com/nino/hrckaraoke/Request.java
+++ src/com/nino/hrckaraoke/Request.java
@@ -2,6 +2,7 @@ package com.nino.hrckaraoke;
 
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -17,6 +18,7 @@ public class Request extends Fragment {
 	Button most_popular,previous_req;
 	
 		
+	@SuppressLint("NewApi")
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -26,7 +28,8 @@ public class Request extends Fragment {
         most_popular=(Button)view.findViewById(R.id.populartab);
         previous_req=(Button)view.findViewById(R.id.prerequesttab);
         
-        
+        most_popular.setBackground(getResources().getDrawable(R.drawable.btn_request_left_active));
+        previous_req.setBackground(getResources().getDrawable(R.drawable.btn_request_right_inactive));
     	FragmentTransaction ft = getFragmentManager().beginTransaction();
 		MostPopularTab fragment = new MostPopularTab();
 		ft.replace(R.id.requestfragment, fragment);
@@ -38,6 +41,9 @@ public class Request extends Fragment {
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
+			
+			 most_popular.setBackground(getResources().getDrawable(R.drawable.btn_request_left_active));
+			 previous_req.setBackground(getResources().getDrawable(R.drawable.btn_request_right_inactive));
 			
 			FragmentTransaction ft = getFragmentManager().beginTransaction();
 			MostPopularTab fragment = new MostPopularTab();
@@ -54,7 +60,8 @@ public class Request extends Fragment {
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			
+			 previous_req.setBackground(getResources().getDrawable(R.drawable.btn_request_right_active));
+			 most_popular.setBackground(getResources().getDrawable(R.drawable.btn_request_left_inactive));
 			FragmentTransaction ft = getFragmentManager().beginTransaction();
 			PreviousTab fragment = new PreviousTab();
 			ft.replace(R.id.requestfragment, fragment);
